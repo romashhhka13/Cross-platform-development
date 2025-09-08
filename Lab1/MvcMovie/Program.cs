ff6a105bf9859e534809a7f7e41727617d354a73
Роман Ильичев <romanilichev@MacBook-Pro-Roman.local>
2025-09-08 12:53:43 +0300

    Создание модели для добавления данных

--- Lab1/MvcMovie/Program.cs
+++ Lab1/MvcMovie/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Models;
 using MvcMovie.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MvcMovieContext>(options =>
@@ -9,6 +10,13 @@ builder.Services.AddDbContext<MvcMovieContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
